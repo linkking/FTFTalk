af33057160e23580fe407658b38dd2a8bd243b0f
zhulei <zhulei@oradt.com>
2016-04-28 13:17:42 +0800

    第一次提交

---
+++ FTFTalk/FTFTalk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FTFTalk
+//
+//  Created by zhulei on 16/4/28.
+//  Copyright © 2016年 oradt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
