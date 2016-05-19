61d9a10542e1ed48809506ee8fdd5429782c8f44
zhulei <zhulei@oradt.com>
2016-05-19 16:20:19 +0800

    第二次提交：修改配置文件。

---
+++ FTFTalk/FTFTalk/FTFHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  FTFHomeViewController.h
+//  FTFTalk
+//
+//  Created by zhulei on 16/5/18.
+//  Copyright © 2016年 oradt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FTFHomeViewController : UIViewController
+
+@end
---
+++ FTFTalk/FTFTalk/FTFHomeViewController.m
@@ -0,0 +1,20 @@
+//
+//  FTFHomeViewController.m
+//  FTFTalk
+//
+//  Created by zhulei on 16/5/18.
+//  Copyright © 2016年 oradt. All rights reserved.
+//
+
+#import "FTFHomeViewController.h"
+
+@implementation FTFHomeViewController
+
+
+-(void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.title = @"head";
+}
+
+@end
